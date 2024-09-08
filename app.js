7bc9d84e389569d297ed942daed60f4d75f23d8e
Isaacfalkoskki <148932970+Isaacfalkoskki@users.noreply.github.com>
2024-09-08 03:07:17 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+const menuResponsive = document.querySelector('.menu-responsive');
+menuResponsive.addEventListener('click', () => {
+    toggleMenu();
+});
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuResponsive.classList.toggle('change');
+
+    if (menuResponsive.classList.contains('change')) {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+
+}
